3efb826a6f7224b32200cd2eab206907bcf45cc8
hunlongyu <hunlongyu@gmail.com>
2016-10-20 18:23:02 +0800

    细节修改
    点击鞭炮，出现失败界面。没有修复。

--- assets/script/GameOne/bianpaoAni.js
+++ assets/script/GameOne/bianpaoAni.js
@@ -2,21 +2,41 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        bianpao:{
+        bianpao: {
+            default: null,
+            type: cc.Node,
+            displayName: 'bianpao',
+        },
+        GameFailure:{
             default:null,
             type:cc.Node,
-            displayName:'bianpao',
+            displayName:'GameFailure',
         },
     },
 
     // use this for initialization
     onLoad: function () {
+        this.GameFailure.active = false;
+
         var bianpao = this.node.getComponent(cc.Animation);
         // bianpao.playAdditive('bianpaoAni');
-        this.bianpao.on('touchstart',function(){
+        this.bianpao.on('touchstart', function () {
             bianpao.playAdditive('bianpaoAni');
+
+
+            // setTimeout(Gf,2);
+            this.GameFailure.active = true;
+        }, this);
+
+        this.GameFailure.on('touchstart',function(){
+            event.stopPropagation();
         },this);
+
     },
+
+    // Gf:function(){
+    //     this.GameFailure.active = true;
+    // },
 
     // called every frame, uncomment this function to activate update callback
     // update: function (dt) {
